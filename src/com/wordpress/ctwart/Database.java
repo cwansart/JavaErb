5db9641f68d07148c61bdbed0eb4bde30d197aaa
Christian Wansart <christian@bluepanel.org>
2015-05-26 00:29:27 +0200

    implenented queries for database structure

--- src/com/wordpress/ctwart/Database.java
+++ src/com/wordpress/ctwart/Database.java
@@ -1,5 +1,70 @@
 package com.wordpress.ctwart;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Database {
 
+	private Connection connection;
+	private Statement statement;
+
+	public Database() {
+		connect();
+		createStructure();
+	}
+
+	private void createStructure() {
+		DatabaseMetaData metaData;
+		try {
+			metaData = connection.getMetaData();
+			ResultSet resultSet = metaData.getTables(null, null, "categories",
+					null);
+
+			if (!resultSet.next()) {
+				statement = connection.createStatement();
+				statement.executeUpdate(getStructureData());
+				statement.close();
+			}
+
+			resultSet.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private String getStructureData() {
+		String createStatement = null;
+		try {
+			createStatement = new String(Files.readAllBytes(Paths
+					.get("data/database-structure.sql")));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return createStatement;
+	}
+
+	private void connect() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			connection = DriverManager.getConnection("jdbc:sqlite:data.db");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- src/com/wordpress/ctwart/Main.java
+++ src/com/wordpress/ctwart/Main.java
@@ -3,8 +3,8 @@ package com.wordpress.ctwart;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		@SuppressWarnings("unused")
+		Database database = new Database();
 	}
 
 }
