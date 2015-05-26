adabd483934d0fdda31149d1725ea45c0878a5c5
Christian Wansart <christian@bluepanel.org>
2015-05-26 10:43:12 +0200

    added params check

--- src/com/wordpress/ctwart/Main.java
+++ src/com/wordpress/ctwart/Main.java
@@ -4,8 +4,17 @@ public class Main {
 
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
+		boolean withTestData = false;
+		for(String arg: args) {
+			if(arg.equals("withTestData")) {
+				withTestData = true;
+			}
+		}
+		
 		Database database = new Database();
-		database.addTestData();
+		if(withTestData) {
+			database.addTestData();
+		}
 		
 		MainWindow window = new MainWindow("JavaERB");
 	}
