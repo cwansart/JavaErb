d2bf34d7a0a27abd36a4823e82838f1a8743f68a
Christian Wansart <christian@bluepanel.org>
2015-05-26 10:30:07 +0200

    create MainWindow in main

--- src/com/wordpress/ctwart/Main.java
+++ src/com/wordpress/ctwart/Main.java
@@ -2,9 +2,12 @@ package com.wordpress.ctwart;
 
 public class Main {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		@SuppressWarnings("unused")
 		Database database = new Database();
+		database.addTestData();
+		
+		MainWindow window = new MainWindow("JavaERB");
 	}
 
 }
